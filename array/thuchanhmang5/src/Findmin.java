b875dc4e6d1727b6f1d1e79752184611b9747777
tung <email@example.com>
2025-04-10 11:13:55 +0700

    timgtnn

---
+++ array/thuchanhmang5/src/Findmin.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Findmin {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in); // nhap thong tin tu ban phim
+        do {
+            System.out.print("Enter a size:"); // vong lap nhap ky tu mang
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size should not exceed 20");
+        } while (size > 20);
+
+        array = new int[size];  // in ra mang moi
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element " + (i+1) + ":");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.print("Property list: "); // in ra mang moi
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+
+        int min = array[0];  // gan max bang vi tri 0
+        int index = 1;
+        for (int j = 0; j < array.length; j++) {   // chay vong lap xac dinh max
+            if (array[j] < min) {
+                min = array[j];
+                index = j + 1;
+            }
+        }
+        System.out.println("Gia tri nho nhat la : " + min + " ,nam o vi tri so  " + index); // in ra ket qua
+    }
+}
