38bfbaf13d2db33e2e412b23d4df3dc41bb1e716
tung <email@example.com>
2025-04-14 17:02:29 +0700

    minitestkethua

---
+++ kethua/kethua-minitest/src/Book.java
@@ -0,0 +1,14 @@
+public class Book {
+    private String bookCode,name,author;
+    private double price;
+
+    public Book(String bookCode, String name, double price ,String author) {
+        this.bookCode = bookCode;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+    public double getPrice() {
+        return price;
+    }
+}
