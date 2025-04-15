3ed42dc603613a5d9d6f5a8fc69b155e7e75e077
tung <email@example.com>
2025-04-15 17:03:17 +0700

    baitapkethua1

---
+++ kethua/baitapkethua1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(2.0, "blue");
+        Cylinder cylinder = new Cylinder(2.0, 5.0, "red");
+
+        System.out.println(circle);
+        System.out.println(cylinder);
+    }
+}
